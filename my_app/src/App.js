0483d107a44af57a4a3e3cabb4ae9d55c13b555e
zeynepthereal <61983919+zeynepthereal@users.noreply.github.com>
2021-05-19 13:21:26 +0300

    Djikstra ile yol bulma

---
+++ my_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react'
+import './App.css';
+import YolBulma from './YolBulma/YolBulma';
+
+function App() {
+  return (
+    <div className="App">
+  <YolBulma></YolBulma>
+    </div>
+  );
+}
+
+export default App;
